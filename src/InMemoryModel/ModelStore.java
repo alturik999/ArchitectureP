f7314b7a678fb3dc2baef33688afdf5287ab4cfe
altur <alturik99@gmail.com>
2023-09-05 20:29:47 +0500

    model

--- src/InMemoryModel/ModelStore.java
+++ src/InMemoryModel/ModelStore.java
@@ -1,4 +1,27 @@
 package InMemoryModel;
 
-public class ModelStore {
+public class ModelStore implements IModelChanger {
+    public PoligonalModel models;
+    public Scene scenes;
+    public Flash flashes;
+    public Camera cameras;
+    private IModelChangeObserver changeObserver;
+
+    public ModelStore(IModelChangeObserver changeObserver) {
+        this.changeObserver = changeObserver;
+
+        this.models = new PoligonalModel(new Poligone());
+        this.flashes = new Flash();
+        this.cameras = new Camera();
+
+        this.scenes = new Scene(this.models, this.flashes, this.cameras);
+    }
+
+    public Scene getScene(int n) {
+        return this.scenes;
+    }
+
+    @Override
+    public void notifyChange(IModelChanger sender) {
+    }
 }
